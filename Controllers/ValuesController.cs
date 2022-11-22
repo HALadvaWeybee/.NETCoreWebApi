722e25d7b21369429254d89f2069e7f8a6ed5368
himanshuLadva <himanshu@webtheuniverse.com>
2022-11-22 18:59:16 +0530

    progresing

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleToWebApi.Controllers
+{
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Route("api/get-all")]
+        public string GetAll()
+        {
+            return "Hello from get all";
+        }
+
+        [Route("api/get-all-author")]
+        public string GetAllAuthor()
+        {
+            return "Hello from get all author";
+        }
+
+        [Route("books/{id}")]
+        public string GetById(int id)
+        {
+            return "Hello" + id;
+        }
+
+        [Route("books/{id}/author/{authorName}")]
+        public string GetAuthorAddressById(int id, string authorName)
+        {
+            return "Hello " + id+ " "+ authorName;
+        }
+
+        [Route("search")]
+        public string SearchBooks(int id, string authorName, string name, int rating, int price)
+        {
+            return "Hello from search";
+        }
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,55 @@
+﻿namespace ConsoleToWebApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // services.AddTransient<CustomMiddleware1>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            /* app.Use(async (context, next) =>
+            {        
+                await context.Response.WriteAsync("hello from use \n");
+                await next();
+                await context.Response.WriteAsync("hello from use 2 \n");
+            });
+
+            app.UseMiddleware<CustomMiddleware1>();
+
+            app.Map("/himanshu", customCode);
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello from run \n");
+            });
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello from run2");
+            }); */
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private void customCode(IApplicationBuilder obj)
+        {
+            obj.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello from custome code \n");
+                await next();
+            });
+        }
+    }
+}
