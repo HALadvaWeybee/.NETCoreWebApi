4ede695e6a95872988fe20c6d1ef709c83f7317e
himanshuLadva <himanshu@webtheuniverse.com>
2022-11-23 18:58:35 +0530

    Complete til 63 videos

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,38 +4,38 @@ using Microsoft.AspNetCore.Mvc;
 namespace ConsoleToWebApi.Controllers
 {
     [ApiController]
+    [Route("[controller]/[action]")]
     public class ValuesController : ControllerBase
     {
-        [Route("api/get-all")]
+        // to disable the base routing put ~ symbool in front of personal routing
+         [Route("~/api/get-all")]
         public string GetAll()
         {
             return "Hello from get all";
         }
 
-        [Route("api/get-all-author")]
+        // [Route("api/get-all-author")]
         public string GetAllAuthor()
         {
             return "Hello from get all author";
         }
 
-        [Route("books/{id}")]
+        [Route("{id}")]
         public string GetById(int id)
         {
             return "Hello" + id;
         }
 
-        [Route("books/{id}/author/{authorName}")]
+        // [Route("books/{id}/author/{authorName}")]
         public string GetAuthorAddressById(int id, string authorName)
         {
             return "Hello " + id+ " "+ authorName;
         }
 
-        [Route("search")]
+        // [Route("search")]
         public string SearchBooks(int id, string authorName, string name, int rating, int price)
         {
             return "Hello from search";
         }
-
-
     }
 }
